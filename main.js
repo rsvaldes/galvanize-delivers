4dacabf68ed1f4c9e111a5d79ed35f82ea6937e2
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-04-30 17:31:50 -0600

    added select forms instead to hold quantites

--- main.js
+++ main.js
@@ -3,100 +3,62 @@ $(document).ready(function(){
     $('.first').hide();
     $('.hidden').show();
   });
-
+  $(function(){
+      // var $select = $(".1-100");
+      for (i=1;i<=100;i++){
+          $('.select').append($('<option></option>').val(i).html(i))
+      }
+  });
 
 
   $('#weinerdawg').one("click", function() {
     $(".table").append($('<tr id = "weinerrow" class ="trow"></tr>'));
     $("#weinerrow").append("<td>Weiner Dawg</td>");
     $("#weinerrow").append("<td>$5</td>");
-    $("#weinerrow").append($('<td id = "weinerquantity" class = "quantity">1</td>'));
+    $('#weinerselect').show();
+    $("#weinerrow").append('<td id = "weinerquantity"></td>');
+    $('#weinerquantity').append($('#weinerselect'));
 
   });
-
   $('#chikagadawg').one("click", function() {
     $(".table").append($('<tr id = "chikagarow" class ="trow"></tr>'));
     $("#chikagarow").append("<td>Chikaga Dawg</td>");
     $("#chikagarow").append("<td>$6</td>");
-    $("#chikagarow").append($('<td id = "chikagaquantity" class = "quantity">1</td>'));
+    $('#chikagaselect').show();
+    $("#chikagarow").append('<td id = "chikagaquantity"></td>');
+    $('#chikagaquantity').append($('#chikagaselect'));
   });
   $('#shillydawg').one("click", function() {
     $(".table").append($('<tr id = "shillyrow" class ="trow"></tr>'));
     $("#shillyrow").append("<td>Shilly Dawg</td>");
     $("#shillyrow").append("<td>$6</td>");
-    $("#shillyrow").append($('<td id = "shillyquantity" class = "quantity">1</td>'));
+    $('#shillyselect').show();
+    $("#shillyrow").append('<td id = "shillyquantity"></td>');
+    $('#shillyquantity').append($('#shillyselect'));
   });
   $('#packledawg').one("click", function() {
     $(".table").append($('<tr id = "packlerow" class ="trow"></tr>'));
     $("#packlerow").append("<td>Packle Dawg</td>");
     $("#packlerow").append("<td>$6</td>");
-    $("#packlerow").append($('<td id = "packlequantity" class = "quantity">1</td>'));
+    $('#packleselect').show();
+    $("#packlerow").append('<td id = "packlequantity"></td>');
+    $('#packlequantity').append($('#packleselect'));
   });
   $('#jalapenodawg').one("click", function() {
     $(".table").append($('<tr id = "jalapenorow" class ="trow"></tr>'));
     $("#jalapenorow").append("<td>Halapena Dawg</td>");
     $("#jalapenorow").append("<td>$6</td>");
-    $("#jalapenorow").append($('<td id = "jalapenoquantity" class = "quantity">1</td>'));
+    $('#jalapenoselect').show();
+    $("#jalapenorow").append('<td id = "jalapenoquantity"></td>');
+    $('#jalapenoquantity').append($('#jalapenoselect'));
   });
   $('#hairydawg').one("click", function() {
     $(".table").append($('<tr id = "hairyrow" class ="trow"></tr>'));
     $("#hairyrow").append("<td>Hairy Dawg</td>");
     $("#hairyrow").append("<td>$5</td>");
-    $("#hairyrow").append($('<td id = "hairyquantity" class = "quantity">1</td>'));
+    $('#hairyselect').show();
+    $("#hairyrow").append('<td id = "hairyquantity"></td>');
+    $('#hairyquantity').append($('#hairyselect'));
   });
 
-  function whole () {
-
-    var hairycount = 0;
-    $('#hairydawg').click(function (){
-      hairycount++;
-      $('#hairyquantity').html(+hairycount);
-    });
-    var jalapenocount = 0;
-    $('#jalapenodawg').click(function (){
-      jalapenocount++;
-      $('#jalapenoquantity').html(+jalapenocount);
-    });
-    var weinercount = 0;
-    $('#weinerdawg').click(function (){
-      weinercount++;
-      $('#weinerquantity').html(+weinercount);
-    });
-    var packlecount = 0;
-    $('#packledawg').click(function (){
-      packlecount++;
-      $('#packlequantity').html(+packlecount);
-    });
-    var chikagacount = 0;
-    $('#chikagadawg').click(function(){
-      chikagacount++;
-      $('#chikagaquantity').html(+chikagacount);
-    });
-
-    var shillycount = 0;
-    $('#shillydawg').click(function(){
-      shillycount++;
-      $('#shillyquantity').html(+shillycount);
-    });
-    var finalShilly = $('#shillyquantity').html();
-    var finalChikaga = $('#chikagaquantity').html();
-    var finalPackle = $('#packlequantity').html();
-    var finalWeiner = $('#weinerquantity').html();
-    var finalHairy = $('#hairyquantity').html();
-    var finalJalapeno = $('#jalapenoquantity').html();
-
-    function getSubtotal () {
-      var q5 = 0;
-      var q6 = 0;
-
-      q5 = (finalWeiner + finalHairy) * 5;
-      q6 = (finalyShilly + finalChikaga + finalJalapeno + finalPackle) * 6;
-      return q6 + q5;
-    }
-var finalNum = getSubtotal();
-
-finalNum = $('#subtotalNum')
-
-}//closing whole
-whole();
 });
