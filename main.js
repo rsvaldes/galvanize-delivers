ce99fd6a8f8ad5d5b02dbb2dc9903e748458f1f9
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-04-29 19:08:56 -0600

    another commit in case i break something

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
 $(document).ready(function(){
-  $('.order-btn').on("click", function () {
-    $('#itemcolumn').append("Dawg");
-    $('#pricecolumn').append("Price");
+  $('.order-btn').on("click", function() {
+    $('.first').hide();
   })
-  $('#weinerdawg').on("click", function() {
-      $('#item').append("Weiner Dawg");
-      $('#price').append("$5");
 
+$('.order-btn').on("click", function () {
+  $('.hidden').show();
+})
+
+  $('#weinerdawg').one("click", function() {
+    $(".table").append($('<tr id = "weinerrow" class ="row"></tr>'));
+    $("#weinerrow").append("<td>Weiner Dawg</td>");
+    $("#weinerrow").append("<td>$5</td>");
+    $("#weinerrow").append($('<td id = "weinerquantity">1</td>'));
 
   })
-  $('#chicagodawg').on("click", function() {
-      $('#item').append("Chikaga Dawg");
-      $('#price').append("$6");
 
-
+  $('#chicagodawg').one("click", function() {
+    $(".table").append($('<tr id = "chikagarow" class ="row"></tr>'));
+    $("#chikagarow").append("<td>Chikaga Dawg</td>");
+    $("#chikagarow").append("<td>$6</td>");
+    $("#chikagarow").append($('<td id = "chikagaquantity">1</td>'));
   })
+  $('#shillydawg').one("click", function() {
+    $(".table").append($('<tr id = "shillyrow" class ="row"></tr>'));
+    $("#shillyrow").append("<td>Shilly Dawg</td>");
+    $("#shillyrow").append("<td>$6</td>");
+    $("#shillyrow").append($('<td id = "shillyquantity">1</td>'));
+  })
+  $('#packledawg').one("click", function() {
+    $(".table").append($('<tr id = "packlerow" class ="row"></tr>'));
+    $("#packlerow").append("<td>Packle Dawg</td>");
+    $("#packlerow").append("<td>$6</td>");
+    $("#packlerow").append($('<td id = "packlequantity">1</td>'));
+  })
+  $('#jalapenodawg').one("click", function() {
+    $(".table").append($('<tr id = "jalapenorow" class ="row"></tr>'));
+    $("#jalapenorow").append("<td>Halapena Dawg</td>");
+    $("#jalapenorow").append("<td>$6</td>");
+    $("#jalapenorow").append($('<td id = "jalapenoquantity">1</td>'));
+  })
+  $('#hairydawg').one("click", function() {
+    $(".table").append($('<tr id = "hairyrow" class ="row"></tr>'));
+    $("#hairyrow").append("<td>Hairy Dawg</td>");
+    $("#hairyrow").append("<td>$5</td>");
+    $("#hairyrow").append($('<td id = "hairyquantity">1</td>'));
+    
+    })
+
+
 });
