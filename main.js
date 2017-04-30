e0f328addaab4bdcfd0b579ead5850f61bce274a
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-04-29 20:29:03 -0600

    again in case i break

--- main.js
+++ main.js
@@ -1,51 +1,78 @@
 $(document).ready(function(){
   $('.order-btn').on("click", function() {
     $('.first').hide();
-  })
+    $('.hidden').show();
+  });
 
-$('.order-btn').on("click", function () {
-  $('.hidden').show();
-})
+
 
   $('#weinerdawg').one("click", function() {
-    $(".table").append($('<tr id = "weinerrow" class ="row"></tr>'));
+    $(".table").append($('<tr id = "weinerrow" class ="trow"></tr>'));
     $("#weinerrow").append("<td>Weiner Dawg</td>");
     $("#weinerrow").append("<td>$5</td>");
-    $("#weinerrow").append($('<td id = "weinerquantity">1</td>'));
+    $("#weinerrow").append($('<td id = "weinerquantity" class = "quantity">1</td>'));
 
-  })
+  });
 
-  $('#chicagodawg').one("click", function() {
-    $(".table").append($('<tr id = "chikagarow" class ="row"></tr>'));
+  $('#chikagadawg').one("click", function() {
+    $(".table").append($('<tr id = "chikagarow" class ="trow"></tr>'));
     $("#chikagarow").append("<td>Chikaga Dawg</td>");
     $("#chikagarow").append("<td>$6</td>");
-    $("#chikagarow").append($('<td id = "chikagaquantity">1</td>'));
-  })
+    $("#chikagarow").append($('<td id = "chikagaquantity" class = "quantity">1</td>'));
+  });
   $('#shillydawg').one("click", function() {
-    $(".table").append($('<tr id = "shillyrow" class ="row"></tr>'));
+    $(".table").append($('<tr id = "shillyrow" class ="trow"></tr>'));
     $("#shillyrow").append("<td>Shilly Dawg</td>");
     $("#shillyrow").append("<td>$6</td>");
-    $("#shillyrow").append($('<td id = "shillyquantity">1</td>'));
-  })
+    $("#shillyrow").append($('<td id = "shillyquantity" class = "quantity">1</td>'));
+  });
   $('#packledawg').one("click", function() {
-    $(".table").append($('<tr id = "packlerow" class ="row"></tr>'));
+    $(".table").append($('<tr id = "packlerow" class ="trow"></tr>'));
     $("#packlerow").append("<td>Packle Dawg</td>");
     $("#packlerow").append("<td>$6</td>");
-    $("#packlerow").append($('<td id = "packlequantity">1</td>'));
-  })
+    $("#packlerow").append($('<td id = "packlequantity" class = "quantity">1</td>'));
+  });
   $('#jalapenodawg').one("click", function() {
-    $(".table").append($('<tr id = "jalapenorow" class ="row"></tr>'));
+    $(".table").append($('<tr id = "jalapenorow" class ="trow"></tr>'));
     $("#jalapenorow").append("<td>Halapena Dawg</td>");
     $("#jalapenorow").append("<td>$6</td>");
-    $("#jalapenorow").append($('<td id = "jalapenoquantity">1</td>'));
-  })
+    $("#jalapenorow").append($('<td id = "jalapenoquantity" class = "quantity">1</td>'));
+  });
   $('#hairydawg').one("click", function() {
-    $(".table").append($('<tr id = "hairyrow" class ="row"></tr>'));
+    $(".table").append($('<tr id = "hairyrow" class ="trow"></tr>'));
     $("#hairyrow").append("<td>Hairy Dawg</td>");
     $("#hairyrow").append("<td>$5</td>");
-    $("#hairyrow").append($('<td id = "hairyquantity">1</td>'));
-    
-    })
+    $("#hairyrow").append($('<td id = "hairyquantity" class = "quantity">1</td>'));
+  });
 
-
+    var hairycount = 0;
+    $('#hairydawg').click(function (){
+      hairycount++;
+      $('#hairyquantity').html(+hairycount);
+    });
+    var jalapenocount = 0;
+    $('#jalapenodawg').click(function (){
+      jalapenocount++;
+      $('#jalapenoquantity').html(+jalapenocount);
+    });
+    var weinercount = 0;
+    $('#weinerdawg').click(function (){
+      weinercount++;
+      $('#weinerquantity').html(+weinercount);
+    });
+    var packlecount = 0;
+    $('#packledawg').click(function (){
+      packlecount++;
+      $('#packlequantity').html(+packlecount);
+    });
+    var chikagacount = 0;
+    $('#chikagadawg').click(function (){
+      chikagacount++;
+      $('#chikagaquantity').html(+chikagacount);
+    });
+    var shillycount = 0;
+    $('#shillydawg').click(function (){
+      shillycount++;
+      $('#shillyquantity').html(+shillycount);
+    });
 });
